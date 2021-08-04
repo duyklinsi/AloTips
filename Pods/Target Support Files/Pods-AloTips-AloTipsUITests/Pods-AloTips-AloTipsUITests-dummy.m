8c90a13ed3d782743cbaff0fa6e6f2504bc3c6cd
duyklinsi <duyptit1986@gmail.com>
2021-08-04 17:38:06 +0700

    init app

---
+++ Pods/Target Support Files/Pods-AloTips-AloTipsUITests/Pods-AloTips-AloTipsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AloTips_AloTipsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AloTips_AloTipsUITests
+@end
---
+++ Pods/Target Support Files/Pods-AloTipsTests/Pods-AloTipsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AloTipsTests : NSObject
+@end
+@implementation PodsDummy_Pods_AloTipsTests
+@end
